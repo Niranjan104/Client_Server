26b7ff80f7c9604cea8e7849460885e80dd5c6d1
Niranjan104 <skilledinfo1@gmail.com>
2026-03-05 16:02:34 +0530

    feat: implement true blue/green ACI deployment with smoke test gate and fixed NGINX entry point

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-# Substitute ${ACTIVE_COLOR} in the nginx config template at container startup
-envsubst '${ACTIVE_COLOR}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-echo "NGINX started with ACTIVE_COLOR=${ACTIVE_COLOR}"
+# Substitute all env vars in the nginx config template at startup
+envsubst '${ACTIVE_COLOR} ${BLUE_SERVER_HOST} ${GREEN_SERVER_HOST} ${BLUE_CLIENT_HOST} ${GREEN_CLIENT_HOST}' \
+  < /etc/nginx/nginx.conf.template \
+  > /etc/nginx/nginx.conf
+
+echo "NGINX started | ACTIVE=${ACTIVE_COLOR} | blue_server=${BLUE_SERVER_HOST} | green_server=${GREEN_SERVER_HOST}"
 exec nginx -g 'daemon off;'
